61ef4002f9df93f33e0fad0c8937675ae9c84183
asfandiyark007 <asfandiyar.bsee261@iiu.edu.pk>
2026-06-14 13:24:38 +0500

    enhance client model & update UI components

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,19 +1,34 @@
 package main
 
-import tea "charm.land/bubbletea/v2"
+import (
+	"strings"
+
+	"charm.land/bubbles/v2/textarea"
+	"charm.land/bubbles/v2/viewport"
+	tea "charm.land/bubbletea/v2"
+)
 
 type model struct {
-	message string
+	messages []string
 	textarea textarea.Model
+	viewport viewport.Model
 }
 
+func initialModel() model {
+	ta := textarea.New()
+	vp := viewport.New(viewport.WithWidth(60), viewport.WithHeight(10))
+	ta.Placeholder = "Type a message..."
+	ta.SetWidth(60)
+	ta.SetHeight(10)
+	ta.SetVirtualCursor(false)
+	ta.Focus()
+	return model{textarea: ta, viewport: vp}
 
-func ()  {
-	
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+
+	return textarea.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -21,19 +36,36 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyPressMsg:
 		switch msg.String() {
+		default:
+			var cmd tea.Cmd
+			m.textarea, cmd = m.textarea.Update(msg)
+			m.viewport, _ = m.viewport.Update(msg)
+			return m, cmd
 		case "ctrl+c", "esc":
 			return m, tea.Quit
+		case "enter":
+			m.messages = append(m.messages, m.textarea.Value())
+			var messages strings.Builder
+			for _, message := range m.messages {
+				messages.WriteString(message)
+				messages.WriteString("\n")
+			}
+			m.viewport.SetContent(messages.String())
+			m.viewport.GotoBottom()
+			m.textarea.Reset()
+			return m, nil
 		}
 	}
 	return m, nil
-}
 
+}
 func (m model) View() tea.View {
-	return tea.NewView("LAN Chat — press ctrl+c to quit\n")
+
+	return tea.NewView("LAN Chat\n\n" + m.textarea.View() + "\nmessages: " + m.viewport.View())
 }
 
 func main() {
-	p := tea.NewProgram(model{})
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module lan-chat
 go 1.26.2
 
 require (
+	charm.land/bubbles/v2 v2.1.0 // indirect
 	charm.land/bubbletea/v2 v2.0.7 // indirect
+	charm.land/lipgloss/v2 v2.0.2 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/charmbracelet/colorprofile v0.4.3 // indirect
 	github.com/charmbracelet/ultraviolet v0.0.0-20260525132238-948f4557a654 // indirect
 	github.com/charmbracelet/x/ansi v0.11.7 // indirect
